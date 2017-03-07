30713d391886ba419f914494e803d30020cf9b10
medtr28 <medtr28@users.noreply.github.com>
2017-03-07 21:58:22 +0900

    add dup

---
+++ ch01/dup1/dup1.go
@@ -0,0 +1,22 @@
+// Dup1: print counts and stdin
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// ignore error handling of input.Err
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
